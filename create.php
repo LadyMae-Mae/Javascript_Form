b13b7dc033ed1ffb5e73b1b20a7d68f9f54671b7
COMLAB <lmrevadenera23-0334@cca.edu.ph>
2025-02-17 19:51:49 +0800

    Update create.php

---
+++ create.php
@@ -0,0 +1,41 @@
+<?php
+	echo "Hello World";
+	
+	var_dump($_POST);
+	$lastname = $_POST['last_name'];
+	$firstname = $_POST['first_name'];
+	
+?>
+
+<br>
+<br>
+<br>
+<br>
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "lorraine";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected Successfully";
+
+$sql = "INSERT INTO students (id, last_name, first_name)
+VALUES ('', '$lastname', '$firstname')";
+
+if ($conn->query($sql) === TRUE) {
+echo "<br>";
+echo "<br>";
+echo "<br>";
+  
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+?>
