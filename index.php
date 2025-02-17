4c70e305775288342bd490775bceff28e1be62a5
COMLAB <jnquinal23-0384@cca.edu.ph>
2025-02-17 18:27:55 +0800

    Update index.php

--- index.php
+++ index.php
@@ -11,14 +11,38 @@
 		<script>
 			const studentTable = document.getElementById("student_table");
 			
-			console.log(studentTable);
 			const row1 = studentTable.insertRow();
-			
 			const row1col1 = row1.insertCell();
 			const row1col2 = row1.insertCell();
 			
 			row1col1.innerHTML = "LAST NAME";
 			row1col2.innerHTML = "FIRST NAME";
+			
+			
+			
+			const row2 = studentTable.insertRow();
+			const row2col1 = row1.insertCell();
+			const row2col2 = row1.insertCell();
+			
+			const inputLastName = document.createElement("input");
+			inputLastName.type="text"
+			inputLastName.placeholder="Insert Last Name";
+			
+			row2col1.append(inputLastName);
+			
+			
+			
+			const inputFirstName = document.createElement("input");
+			inputFirstName.type="text"
+			inputFirstName.placeholder="Insert First Name";
+			
+			row2col2.append(inputFirstName);
+			
+			const row3 = studentTable.insertRow();
+			const row3col1 = row3.inserCell();
+			row3col1.colSpan="2";
+			row3col1.innerHTML="TEST";
+			
 		</script>
 	</body>
 </html>
