28ead1ec77e434362cbb7aeb2d6a66b763b78d48
COMLAB <jnquinal23-0384@cca.edu.ph>
2025-02-17 18:10:30 +0800

    Update index.php

--- index.php
+++ index.php
@@ -12,6 +12,13 @@
 			const studentTable = document.getElementById("student_table");
 			
 			console.log(studentTable);
+			const row1 = studentTable.insertRow();
+			
+			const row1col1 = row1.insertCell();
+			const row1col2 = row1.insertCell();
+			
+			row1col1.innerHTML = "LAST NAME";
+			row1col2.innerHTML = "FIRST NAME";
 		</script>
 	</body>
 </html>
